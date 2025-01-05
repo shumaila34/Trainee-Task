dfef04fe3fabdd5a81f2d2da23ba9b5374079b9b
Shumaila <shumaila.1592@gmail.com>
2025-01-05 14:39:52 +0500

    Redux toolkit store configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Store } from "./redux/store";
+import { Store } from "./Store/store";
 
 import App from "./App";
 
